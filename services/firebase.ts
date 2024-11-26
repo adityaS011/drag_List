083bb7db9839418d6f5722070e81869870cae83e
Aditya Singh <adityasinghtech011@gmail.com>
2024-11-26 21:01:29 +0530

    initial

--- services/firebase.ts
+++ services/firebase.ts
@@ -10,13 +10,13 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDr23ge7KJRElbFK7fCKQevXAM2DovGuoI',
-  authDomain: 'list-status.firebaseapp.com',
-  projectId: 'list-status',
-  storageBucket: 'list-status.firebasestorage.app',
-  messagingSenderId: '409911416011',
-  appId: '1:409911416011:web:269ff766aec4ba08c3374e',
-  measurementId: 'G-1F2KP6RDZ7',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
